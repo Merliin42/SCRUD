2ceeb9242d4958077655db617d8f5606add9fb84
Merliin <lucasdupuis7@gmail.com>
2018-12-28 22:08:00 +0100

    Correction de la direction d'affichage du menu

--- index.php
+++ index.php
@@ -8,33 +8,31 @@
 <body class="container justify-content-center">
 	<form action="Backend/login.php" method="post" class="jumbotron">
 		<h1 class="display-3 text-center">Connexion</h1>
-		<fieldset>
-			<div class="row">
-				<div class="col-lg-1"></div>
-				<div class="form-group col-lg-3">
-					<label for="connectHost" class="col-lg-2 col-form-label">Host</label>
-					<div class="col-lg-10">
-						<input type="text" class="form-control" name="connectHost" id="connectHost">
-					</div>
-				</div>
-				<div class="form-group col-lg-3">
-					<label for="connectUser" class="col-lg-2 col-form-label">Utilisateur</label>
-					<div class="col-lg-10">
-						<input type="text" class="form-control" name="connectUser" id="connectUser">
-					</div>
-				</div>
-				<div class="form-group col-lg-3">
-					<label for="connectPassword" class="col-form-label col-lg-12">Mot de passe</label>
-					<div class="col-lg-10">
-						<input type="password" class="form-control" name="connectPassword" id="connectPassword">
-					</div>
-				</div>
-				<div class="form-group col-lg-2">
-					<div class="col-lg-12" style="height: 2.25rem"></div>
-					<input type="submit" name="submit" class="btn btn-primary align-self-end">
+		<div class="form-inline">
+			<div class="col-lg-1"></div>
+			<div class="form-group col-lg-3">
+				<label for="connectHost" class="col-lg-12 col-form-label">Host</label>
+				<div class="col-lg-10">
+					<input type="text" class="form-control" name="connectHost" id="connectHost">
 				</div>
 			</div>
-		</fieldset>
+			<div class="form-group col-lg-3">
+				<label for="connectUser" class="col-lg-12 col-form-label">Utilisateur</label>
+				<div class="col-lg-10">
+					<input type="text" class="form-control" name="connectUser" id="connectUser">
+				</div>
+			</div>
+			<div class="form-group col-lg-3">
+				<label for="connectPassword" class="col-form-label col-lg-12">Mot de passe</label>
+				<div class="col-lg-10">
+					<input type="password" class="form-control" name="connectPassword" id="connectPassword">
+				</div>
+			</div>
+			<div class="form-group col-lg-2">
+				<div class="col-lg-12" style="height: 2.25rem"></div>
+				<input type="submit" name="submit" class="btn btn-primary align-self-end">
+			</div>
+		</div>
 	</form>
 </body>
 </html>
