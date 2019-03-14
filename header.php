473925dae6efbaaeec689763ca420210c68bac86
Lucas Dupuis <lucasdupuis7@gmail.com>
2019-03-14 10:17:43 +0100

    Suppression test

--- header.php
+++ header.php
@@ -1,6 +1,5 @@
 <?php 
 include "Backend/functions.php";
-echo "Test bootstrap";
 ?>
 <!DOCTYPE html>
 <html>
@@ -8,7 +7,6 @@ echo "Test bootstrap";
 	<title>SCRUD</title>
 	<link rel="stylesheet" type="text/css" href="/Style/bootstrap.min.css">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-	<!-- <link rel="stylesheet" type="text/css" href="/Style/style.css"> -->
 </head>
 <body>
 <nav class="navbar navbar-dark bg-primary">
